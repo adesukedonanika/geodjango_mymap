ce9ba7a5aba206fc3442dbcef2ecb09f80a06d30
adesu-Win10NoteGPU <adesukedonanika@gmail.com>
2021-06-29 22:08:13 +0900

    gdal Path setting.py

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -128,3 +128,22 @@ STATIC_URL = '/static/'
 
 
 
+
+import os
+if os.name == 'nt':
+    import platform
+    POSTGRES = r"C:\Program Files\PostgreSQL\13"
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['POSTGRES_ROOT'] = POSTGRES
+    os.environ['GDAL_LIBRARY_PATH'] = OSGEO4W + r"\bin"
+    os.environ['GEOS_LIBRARY_PATH'] = OSGEO4W + r"\bin"
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + POSTGRES + r"\bin;" + os.environ['PATH']
+    
+    
